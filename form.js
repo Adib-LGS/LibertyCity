5c55760006d80ff0b242d6f7ccf6382fdae4d738
Adib-Legastelois <adib.legastelois@icloud.com>
2020-03-26 18:17:24 +0100

    Third CSS FIX STYLE COMMIT

--- form.js
+++ form.js
@@ -9,8 +9,7 @@ class Form {
         this.reservationBtn = document.querySelector("#resaBtn");
         this.validerButton = document.querySelector("#button-valider");
         this.annulerButton = document.querySelector('#button-annuler');
-        this.myRegex = /^[a-zA-Z-\s]+$/;
-        
+        this.myRegex = /^[a-zA-Z-\s]+$/;   
     }
 
     /* Verification Users Input avec REGEX*/
@@ -22,19 +21,21 @@ class Form {
                 this.errorForm.style.color = 'red';
                 this.errorForm.style.display = 'block';
                 this.reservationBtn.style.display = 'none';
-                //trim() == function qui empeche prise en compte des "espaces" comme valeur ds Formulaire
+
+            //trim() == function qui empeche prise en compte des "espaces" comme valeur ds Formulaire
             } else if ((!this.nomForm.value.trim()) || (!this.prenomForm.value.trim())) {
                 this.errorForm.innerHTML = "SVP, veuillez remplir tout les champs";
                 this.errorForm.style.color = 'orange';
                 this.errorForm.style.display = 'block';
                 this.reservationBtn.style.display = 'none';
+
             } else if ((!localStorage.getItem(this.nomForm.value)) && (!localStorage.getItem(this.prenomForm.vale))) {
                 localStorage.setItem('Nom', this.nomForm.value);
                 localStorage.setItem('Prenom', this.prenomForm.value);
                 this.reservationBtn.style.display = 'block';
                 this.errorForm.style.display = 'none';
             }   
-            });
+        });
     }
 
     /* Recupere le Nom et Prenom dans formVerification et le colle dans le Formulaire*/
@@ -42,11 +43,9 @@ class Form {
         this.myForm.addEventListener("click", (e) => {
             e.preventDefault();
             this.nomForm.value = localStorage.getItem('Nom');
-            this.prenomForm.value = localStorage.getItem('Prenom');
-        
+            this.prenomForm.value = localStorage.getItem('Prenom');   
         });
     }
-
 
     /* Refait apparaite le Block d'info et fait disparaitre le Canvas*/
     formValidation() {
